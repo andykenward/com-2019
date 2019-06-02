a6d89ca63137b6e50486026824e1fa92fefccc90
Andy Kenward <hello@andykenward.com>
2019-06-02 21:36:08 +0100

    feat: storybook

---
+++ src/components/__stories__/typography.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import { storiesOf } from "@storybook/react"
+
+import { Body } from "../typography"
+
+storiesOf("Typography", module).add("<Body />", () => <Body> Body</Body>)
