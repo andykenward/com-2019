60c8ae4f98faf02f89b5c770a91a4f379553f5e1
Andy Kenward <hello@andykenward.com>
2019-06-07 12:14:12 +0100

    feat: remove test files for action for now

--- action-deploy/src/__tests__/get-alias.js
+++
@@ -1,7 +0,0 @@
-const getAlias = require('../get-alias')
-
-describe('getAlias()', () => {
-  it('works', () => {
-    expect(getAlias('@primer/css', 'add-foo')).toEqual('primer-css-add-foo.now.sh')
-  })
-})
