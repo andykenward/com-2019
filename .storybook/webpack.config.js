04057573376d51fbfbf8a6405b27b009431df8c8
Andy Kenward <hello@andykenward.com>
2019-06-09 19:22:15 +0100

    fix: storybook emotion inline `css`

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = ({ config }) => {
     require.resolve("@babel/preset-react"),
     require.resolve("@babel/preset-env"),
     require.resolve("@babel/preset-typescript"),
+    require.resolve("@emotion/babel-preset-css-prop"),
   ]
 
   config.module.rules[0].use[0].options.plugins = [
