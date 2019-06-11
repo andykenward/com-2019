ade94821d687b8453f04c3df80fa24b68db3d911
Andy Kenward <hello@andykenward.com>
2019-06-11 21:01:01 +0100

    feat: primary color change

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,9 +1,12 @@
 export const colors = {
-  primary: "rgb(249, 224, 259)",
+  primary: "rgb(216,234,204)",
   black: "rgb(36, 28, 21)",
   oyster: "rgb(64,59,59)",
   white: "#fff",
   green: "#007c89",
+  squash: "rgb(251,238,202)",
+  cucumber: "rgb(216,234,204)",
+  radish: "rgb(249,224,250)",
 } as const
 
 export type ColorKeys = keyof typeof colors
