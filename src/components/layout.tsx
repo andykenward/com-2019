f1b19a730be50e38c4763048d75398f7992f3baf
Andy Kenward <hello@andykenward.com>
2019-06-11 21:33:36 +0100

    fix: emotion duplicate stylesheet classnames

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,17 +11,18 @@ import Header from "./header"
 import { Footer } from "./footer"
 import { useContact } from "./contact"
 import { useHeader } from "./header/query"
+import { Box } from "./box"
 
 interface Props {
   children: ReactNode
 }
 
 const Layout: React.FC<Props> = ({ children }) => (
-  <>
+  <Box>
     <Header data={useHeader()} />
     <main>{children}</main>
     <Footer data={useContact()} />
-  </>
+  </Box>
 )
 
 export default Layout
