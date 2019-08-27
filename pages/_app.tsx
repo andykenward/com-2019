bb2b7b68236391d80101376ff06c6717f8af19dd
Andy Kenward <hello@andykenward.com>
2019-08-27 10:20:03 +0100

    fix: next js app container deprecated
    https://github.com/zeit/next.js/blob/master/errors/app-container-deprecated.md

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-import { ApolloProvider } from "@apollo/react-hooks"
-import { ThemeProvider } from "emotion-theming"
-import { AppPropsType } from "next-server/dist/lib/utils"
-import { AppInitialProps, Container } from "next/app"
-import Router from "next/router"
-import * as React from "react"
-import { pageview } from "../lib/gtag"
-import { theme } from "../lib/theme"
-import withApolloClient, { ApolloProps } from "../lib/with-apollo-client"
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ThemeProvider } from "emotion-theming";
+import { AppPropsType } from "next-server/dist/lib/utils";
+import { AppInitialProps } from "next/app";
+import Router from "next/router";
+import * as React from "react";
+import { pageview } from "../lib/gtag";
+import { theme } from "../lib/theme";
+import withApolloClient, { ApolloProps } from "../lib/with-apollo-client";
 
 export const config = { amp: `hybrid` }
 class MyApp extends React.Component<
   ApolloProps & AppInitialProps & AppPropsType
-> {
+  > {
   render() {
     const { Component, pageProps, apolloClient, ...others } = this.props
     return (
-      <Container>
-        <ApolloProvider client={apolloClient}>
-          <ThemeProvider theme={theme}>
-            <Component {...pageProps} {...others} />
-          </ThemeProvider>
-        </ApolloProvider>
-      </Container>
+
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} {...others} />
+        </ThemeProvider>
+      </ApolloProvider>
+
     )
   }
 }
