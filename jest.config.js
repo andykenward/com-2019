28714853604b6871b29610bcf39990d7394f4873
Andy Kenward <hello@andykenward.com>
2019-09-01 19:39:25 +0100

    chore: jest ignore more folders

--- jest.config.js
+++ jest.config.js
@@ -147,7 +147,12 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/out/",
+    "<rootDir>/coverage/",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
