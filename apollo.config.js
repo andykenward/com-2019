91809995f117640d7f1bbb755683215f7719f00f
Andy Kenward <hello@andykenward.com>
2019-09-05 08:50:21 +0100

    feat: offline schema generation

--- apollo.config.js
+++ apollo.config.js
@@ -1,14 +1,11 @@
 require("dotenv").config()
-const SCHEMA_PATH =
-  process.env.SCHEMA_PATH ||
-  "https://api-euwest.graphcms.com/v1/cjywu05hg0ysg01dj8nkna8ma/master"
 
 module.exports = {
   client: {
     includes: ["./components/**/*.ts"],
     service: {
       name: "graphcms",
-      url: SCHEMA_PATH,
+      url: process.env.SCHEMA_PATH,
     },
   },
 }
