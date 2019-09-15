07bf0a97d3fe11c4423762aafb241b6563180897
Andy Kenward <hello@andykenward.com>
2019-09-15 20:08:07 +0100

    fix: global css first paint
    Signed-off-by: Andy Kenward <hello@andykenward.com>

--- components/head/index.tsx
+++ components/head/index.tsx
@@ -1,5 +1,3 @@
-import { css, Global } from "@emotion/core"
-import emotionNormalize from "emotion-normalize"
 import NextHead from "next/head"
 
 interface Props {
@@ -11,37 +9,36 @@ export const Head: React.FC<Props> = ({
   title = "Andy Kenward - Senior Front-End Engineer",
   description = "Andy Kenward, Senior Front-End Engineer in London, England",
 }) => (
-  <>
-    <Global
-      styles={css`
-        @import "/static/fonts/index.css";
-        ${emotionNormalize}
-        html {
-          box-sizing: border-box;
-          font-family: "Open Sans", "serif";
-          -ms-text-size-adjust: 100%;
-          -webkit-text-size-adjust: 100%;
-        }
-        *,
-        *:before,
-        *:after {
-          box-sizing: inherit;
-        }
-        body {
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        }
-      `}
-    />
-    <NextHead>
-      <title>{title}</title>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      ></meta>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-    </NextHead>
-  </>
+  <NextHead>
+    <title>{title}</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    ></meta>
+    <meta name="description" content={description} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <link rel="stylesheet" href="/static/normalize.css" />
+    <style>
+      {`
+          html {
+            box-sizing: border-box;
+            font-family: "Open Sans", "serif";
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+          body {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+    </style>
+
+    <link rel="stylesheet" href="/static/fonts/index.css" />
+  </NextHead>
 )
