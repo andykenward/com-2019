bece9e80ca6d7724f18ff45a8229370ec2c0f074
Andy Kenward <hello@andykenward.com>
2019-10-30 07:05:52 +0000

    feat: only load analytics in production

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,27 +5,33 @@ import React from "react"
 
 import { GA_TRACKING_ID } from "../lib/gtag"
 
+const IS_DEV = process.env.NODE_ENV === "development"
+
 export default class extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+        {!IS_DEV && (
+          <Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', '${GA_TRACKING_ID}');
           `,
-            }}
-          />
+              }}
+            />
+          </Head>
+        )}
+        <Head>
           <link rel="stylesheet" href="/static/normalize.css" />
           <link rel="stylesheet" href="/static/fonts/index.css" />
           <Global
