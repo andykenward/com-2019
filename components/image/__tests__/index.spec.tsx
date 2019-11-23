1ca97b99136108f5cc70e1374129299f44c48c31
Andy Kenward <hello@andykenward.com>
2019-11-23 17:12:53 +0000

    wip: image

---
+++ components/image/__tests__/index.spec.tsx
@@ -0,0 +1,27 @@
+describe("<Image />", () => {
+  //     <picture>
+  // <!--[if IE 9]><video style="display: none;"><![endif]-->
+  // <source media="(min-width: 980px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: 980px) and (min-resolution: 120dpi)" sizes="620px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=620&amp;quality=45&amp;auto=format&amp;fit=max&amp;dpr=2&amp;s=4fef8b0ef11e3fd9520485658a45b222 1240w">
+  // <source media="(min-width: 980px)" sizes="620px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=620&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=597d4b3163e10913ba22ab816e7f17bf 620w">
+  // <source media="(min-width: 740px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: 740px) and (min-resolution: 120dpi)" sizes="700px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=700&amp;quality=45&amp;auto=format&amp;fit=max&amp;dpr=2&amp;s=767b7ab4824db3b1b313fa9e0e9110b1 1400w">
+  // <source media="(min-width: 740px)" sizes="700px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=700&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=a0db42e703a3699b2713f6b4e18df907 700w">
+  // <source media="(min-width: 660px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: 660px) and (min-resolution: 120dpi)" sizes="620px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=620&amp;quality=45&amp;auto=format&amp;fit=max&amp;dpr=2&amp;s=4fef8b0ef11e3fd9520485658a45b222 1240w">
+  // <source media="(min-width: 660px)" sizes="620px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=620&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=597d4b3163e10913ba22ab816e7f17bf 620w">
+  // <source media="(min-width: 480px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: 480px) and (min-resolution: 120dpi)" sizes="645px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=645&amp;quality=45&amp;auto=format&amp;fit=max&amp;dpr=2&amp;s=a904a1c69d7d67b21a508dbfb20f2de5 1290w">
+  // <source media="(min-width: 480px)" sizes="645px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=645&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=4b48393c53e05a5aef5cabccef76a345 645w">
+  // <source media="(min-width: 0px) and (-webkit-min-device-pixel-ratio: 1.25), (min-width: 0px) and (min-resolution: 120dpi)" sizes="465px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=465&amp;quality=45&amp;auto=format&amp;fit=max&amp;dpr=2&amp;s=17af76d703e51158c014ee856f08d440 930w">
+  // <source media="(min-width: 0px)" sizes="465px" srcset="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=465&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=5c3f7c7356bb58c638e11ee9d1f26b3c 465w">
+  // <!--[if IE 9]></video><![endif]-->
+  // <img class="maxed responsive-img" itemprop="contentUrl" alt="Prince Andrew and his mother attend a Pitch@Palace event at St James’s Palace in London." src="https://i.guim.co.uk/img/media/4588afed2232ff982a0dbc20242e9a901935a9e4/418_1034_2185_1311/master/2185.jpg?width=300&amp;quality=85&amp;auto=format&amp;fit=max&amp;s=f925351c7638e9e883c98fede08cc732">
+  // </picture>
+  it.todo("should render correctly")
+  it.todo("should render srcset")
+
+  it.todo("should render type image/webp")
+
+  it.todo("should render type image/jpeg")
+
+  it.todo("should render pixel-ratio")
+
+  it.todo("should render media")
+})
