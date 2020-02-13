2aafe6bd3ae04c5d5a8f24425db6bf3ae2f44936
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-02-13 10:12:06 +0000

    fix: now not deploying assets folder
    fixup! fix: now not deploying assets folder
    
    fixup! fix: now not deploying assets folder
    
    fixup! fix: now not deploying assets folder
    
    fixup! fix: now not deploying assets folder
    
    fixup! fix: now not deploying assets folder

--- components/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ThemeProvider } from "emotion-theming"
-
-import { theme } from "../../lib/theme"
-
-export const App: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-)
