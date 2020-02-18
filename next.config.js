e43fea57b43f6c56e787c8f81e477559a289de16
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-02-18 22:17:50 +0000

    feat: normalize.css hashed

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   webpack(config) {
     config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      test: /\.(woff|woff2|eot|ttf|otf|css)$/,
       use: [
         {
           loader: "file-loader",
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,3 @@
-import { Global } from "@emotion/core"
-import css from "@emotion/css"
 import Document, { Head, Main, NextScript } from "next/document"
 import React from "react"
 
@@ -7,29 +5,7 @@ export default class extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>
-          <link rel="stylesheet" href="/static/normalize.css" />
-          <Global
-            styles={css`
-              html {
-                box-sizing: border-box;
-                font-family: "Open Sans", "serif";
-                -ms-text-size-adjust: 100%;
-                -webkit-text-size-adjust: 100%;
-              }
-              *,
-              *:before,
-              *:after {
-                box-sizing: inherit;
-              }
-              body {
-                -webkit-font-smoothing: antialiased;
-                -moz-osx-font-smoothing: grayscale;
-              }
-            `}
-          />
-        </Head>
-
+        <Head />
         <body>
           <Main />
           <NextScript />
