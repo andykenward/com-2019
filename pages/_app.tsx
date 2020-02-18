fb5d1b34823be9c4064a5f2ff9c6eb6bbcaa0164
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-02-18 22:17:50 +0000

    feat: normalize.css hashed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
+import { Global } from "@emotion/core"
+import css from "@emotion/css"
 import { ThemeProvider } from "emotion-theming"
 import { AppProps } from "next/app"
+import Head from "next/head"
 
+import Normalize from "../assets/normalize.css"
 import { Fonts } from "../components/fonts"
 import { theme } from "../lib/theme"
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <link rel="stylesheet" href={`${Normalize}`} />
+    </Head>
     <Fonts />
+    <Global
+      styles={css`
+        html {
+          box-sizing: border-box;
+          font-family: "Open Sans", "serif";
+          -ms-text-size-adjust: 100%;
+          -webkit-text-size-adjust: 100%;
+        }
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+        body {
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+      `}
+    />
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
     </ThemeProvider>
