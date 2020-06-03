65a648603999d096d093fb85fa093915f4570fc9
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-06-03 22:03:35 +0100

    feat: remove testing framework

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint',
-    "plugin:jest/recommended",
-    "plugin:jest-dom/recommended",
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "plugin:import/errors",
     "plugin:import/warnings",
@@ -16,8 +14,6 @@ module.exports = {
   plugins: [
     "@typescript-eslint",
     "react",
-    "jest",
-    "jest-dom",
     "simple-import-sort",
     "prettier",
     "import",
