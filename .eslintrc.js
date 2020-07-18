8ad8fea80a544a002dc52d927abc632758c1bddd
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-07-18 11:16:45 +0100

    wip: jest setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,23 @@ module.exports = {
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:jest/all",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
+    "plugin:testing-library/recommended",
+    "plugin:testing-library/react",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: [
     "@typescript-eslint",
+    "import",
+    "jest",
+    "prettier",
     "react",
     "simple-import-sort",
-    "prettier",
-    "import",
+    "testing-library",
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
