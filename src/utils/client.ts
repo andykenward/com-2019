4cf64d6e335b5a203acd0faacd0d624c8aaa5563
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2020-10-30 20:15:12 +0000

    feat: msw mock server

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,15 +2,25 @@ import { GraphQLClient } from "graphql-request"
 
 import { getSdk } from "../../generated/graphql"
 
-if (!process.env.API_URL || !process.env.API_TOKEN) {
+const API_URL =
+  process.env.NODE_ENV === "test" && !process.env.API_URL
+    ? "http://localhost:3000/graphql"
+    : process.env.API_URL
+
+const API_TOKEN =
+  process.env.NODE_ENV === "test" && !process.env.API_TOKEN
+    ? "test"
+    : process.env.API_TOKEN
+
+if (!API_URL || !API_TOKEN) {
   throw Error(
     "API_URL or API_TOKEN are missing, please define in environment variables"
   )
 }
 
-const graphqlClient = new GraphQLClient(process.env.API_URL, {
+const graphqlClient = new GraphQLClient(API_URL, {
   headers: {
-    authorization: `Bearer ${process.env.API_TOKEN}`,
+    authorization: `Bearer ${API_TOKEN}`,
   },
 })
 
