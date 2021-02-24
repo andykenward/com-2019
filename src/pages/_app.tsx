7a88bd00af680a1e85e11b2b940a836da6538963
andykenward <4893048+andykenward@users.noreply.github.com>
2021-02-24 22:29:29 +0000

    feat: enable js on production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,42 +2,9 @@ import "../../public/fonts/index.css"
 import "../../styles/globals.css"
 
 import { AppProps } from "next/app"
-import { useEffect } from "react"
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
-  useEffect(() => {
-    // https://github.com/zeit/next.js/issues/3303
-    if ("scrollRestoration" in window.history) {
-      window.history.scrollRestoration = "manual"
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const cachedScrollPositions: Array<any> = []
-      let shouldScrollRestore: { x: number; y: number } | null
-
-      router.events.on("routeChangeStart", () => {
-        if (!shouldScrollRestore) {
-          cachedScrollPositions.push([window.scrollX, window.scrollY])
-        }
-      })
-
-      router.events.on("routeChangeComplete", () => {
-        if (shouldScrollRestore) {
-          const { x, y } = shouldScrollRestore
-          window.scrollTo(x, y)
-          shouldScrollRestore = null
-        }
-      })
-
-      router.beforePopState(() => {
-        if (cachedScrollPositions.length > 0) {
-          const [x, y] = cachedScrollPositions.pop()
-          shouldScrollRestore = { x, y }
-        }
-        return true
-      })
-    }
-  }, [router])
-
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+)
 
 export default MyApp
